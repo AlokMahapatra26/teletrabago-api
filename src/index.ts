36d79ead1fdbacdbb82d3fea9de48562f295bd9e
Alok Mahapatra <alokmahapatra2604@gmail.com>
2025-10-07 01:53:18 +0530

    wb fix

--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://teletrabago.vercel.app/', 
-    process.env.RENDER_EXTERNAL_URL || '*',
+    'https://teletrabago.vercel.app', 
+     process.env.RENDER_EXTERNAL_URL,
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
